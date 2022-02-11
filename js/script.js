522727c48aa075cfdc049ee32168962474530193
eorthus <oakenshielddis@gmail.com>
2022-02-12 00:18:44 +0300

    header+footer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-function Visible(event) {
-    let item = document.getElementById(`${event}`);
-    item.classList.toggle('hidden')
-}
+$(document).ready(function () {
+    var $slider = $('.header-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        adaptiveHeight: true
+    });
+
+});
+
